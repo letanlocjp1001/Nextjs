799939d851bcb9de027854dd1dc8cf171c9b7a6b
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-15 02:30:09 +0900

    router and Link

---
+++ pages/clients/index.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+
+function ClientsPage() {
+  const clients = [
+    {
+      id: 'loc',
+      name: 'Le Tan Loc',
+    },
+    {
+      id: 'dung',
+      name: 'Tran Ngoc Dang Dung',
+    },
+    {
+      id: 'tai',
+      name: 'Le Tan Tai',
+    },
+  ]
+
+  return (
+    <div>
+      <h1>The Clients Pages</h1>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ClientsPage
