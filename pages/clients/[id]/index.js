731ad47904c1280cd9a2b0794d81d8a1ffbeb1a7
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-15 11:24:12 +0900

    finish router Next.js

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,17 @@ function ClientProjectsPage() {
 
   console.log(router.query)
 
+  const loadProjectHandler = () => {
+    // router.push('/clients/tai/projecta')
+    router.push({
+      pathname: '/clients/[id]/[clientproject]',
+      query: { id: 'tai', clientproject: 'projecta' },
+    })
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
